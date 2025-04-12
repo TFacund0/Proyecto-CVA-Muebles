21600e98d4f94517a4aead3f78aa07e2b3f146ee
Tobias <tobiascesarfacundoacevedo@gmail.com>
2025-04-12 19:26:06 -0300

    Añadi una version no completa del navegador

--- app/Views/plantilla.php
+++ app/Views/plantilla.php
@@ -6,12 +6,62 @@
     
     <title>CVA MUEBLES</title>
     
-    <link rel="stylesheet" href="assets/css/miestilo2.css">
+    <link rel="stylesheet" href="assets/css/miestilo.css">
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <script src="assets/js/bootstrap.bundle.min.js" ></script>
   
 </head>
   <body>
-    
-</body>
+    <!--BARRA DE NAVEGACION-->
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+
+        <!-- Logo de la barra de navegacion -->
+        <img src="assets/img/logo/cva2.png" alt="" width="50px" class="me-3">
+        <a class="navbar-brand" href="#">Muebles</a>
+
+        <!-- Botón para dispositivos pequeños -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" 
+          aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Contenedor del menú -->
+        <div class="collapse navbar-collapse justify-content-between" id="navbarContent">
+          
+          <!-- Menú centrado -->
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Productos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Ofertas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Más Información</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contactanos</a>
+            </li>
+          </ul>
+
+          <!-- Links a la derecha -->
+          <div class="d-flex">
+            <a class="nav-link" href="#">
+              <img src="assets/img/iconos/cart-check.svg" alt="" class="icono">
+            </a>
+            
+            <a class="nav-link" href="#">
+              <img src="assets/img/iconos/person.svg" alt="" class="icono">
+            </a>
+          </div>
+        </div>
+
+      </div>
+    </nav>
+
+  </body>
 </html>
