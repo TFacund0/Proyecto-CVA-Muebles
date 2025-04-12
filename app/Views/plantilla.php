f35e1a963cad4dfb7eebe6d31ed5988f1a874a0b
Tobias <tobiascesarfacundoacevedo@gmail.com>
2025-04-12 16:45:42 -0300

    Inicializando con todos mis archivos de mi computadora

---
+++ app/Views/plantilla.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>CVA MUEBLES</title>
+    
+    <link rel="stylesheet" href="assets/css/miestilo2.css">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <script src="assets/js/bootstrap.bundle.min.js" ></script>
+  
+</head>
+  <body>
+    
+</body>
+</html>
