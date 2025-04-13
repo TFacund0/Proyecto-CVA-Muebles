ffa18f360f0ddc9dec6b18732f934db748be3b65
Tobias <tobiascesarfacundoacevedo@gmail.com>
2025-04-13 08:12:12 -0300

    No hice ningun cambio, solo son las cosas del debuger que piden ser commiteadas

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,29 +4,8 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function __construct()
+    public function index(): string
     {
-        helper(['url', 'date']); // Carga los helpers
-    }
-
-    public function index()
-    {
-
-        $footerData = [
-        'company_name'   => 'CVA Muebles',
-        'company_address'=> '9 de Julio 1449, Ctes, Argentina',
-        'phone'         => '+54 9 3794 098511',
-        'social_links'  => [
-            'facebook'  => 'https://fb.com/misitio',
-            'twitter'   => 'https://twitter.com/misitio',
-            'instagram' => 'https://instagram.com/misitio',
-        ],
-        'current_year'  => date('Y') // Usa el helper 'date'
-        ];
-
-        // 2. Construye la vista final ensamblando todo
-        return view('front/navbar')
-            . view('plantilla', ['title' => 'CVA Muebles'])
-            . view('front/footer', $footerData);
+        return view('plantilla.php');
     }
 }
