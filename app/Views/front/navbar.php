886611666463bd7f3b797f05ba7f91f2a4681e0a
fer <fernandoarceobregon@gmail.com>
2025-04-13 01:54:22 -0300

    Guardo archivos esenciales: iconos SVG, vistas front y .gitignore

---
+++ app/Views/front/navbar.php
@@ -0,0 +1,52 @@
+<nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+
+        <!-- Logo de la barra de navegacion -->
+        <div class="content-logo">
+          <img src="assets/img/logo/cva2.png" alt="" width="50px" class="logo ms-2 me-2">
+        </div>
+        
+        <a class="navbar-brand" href="#"><span class="titulo-logo">CVA Muebles</span></a>
+
+        <!-- Botón para dispositivos pequeños -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" 
+          aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Contenedor del menú -->
+        <div class="collapse navbar-collapse justify-content-between" id="navbarContent">
+          
+          <!-- Menú centrado -->
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Productos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Ofertas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Más Información</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contactanos</a>
+            </li>
+          </ul>
+
+          <!-- Links a la derecha -->
+          <div class="d-flex">
+            <a class="nav-link" href="#">
+              <img src="assets/img/iconos/cart-check.svg" alt="" class="icono">
+            </a>
+            
+            <a class="nav-link" href="#">
+              <img src="assets/img/iconos/person.svg" alt="" class="icono">
+            </a>
+          </div>
+        </div>
+
+      </div>
+    </nav>
