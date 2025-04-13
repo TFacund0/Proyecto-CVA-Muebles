000bc09cba936516869a25001d193f744468d153
fer <fernandoarceobregon@gmail.com>
2025-04-13 01:39:53 -0300

    cosas que no quiero perder

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,29 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function __construct()
     {
-        return view('plantilla.php');
+        helper(['url', 'date']); // Carga los helpers
+    }
+
+    public function index()
+    {
+
+        $footerData = [
+        'company_name'   => 'CVA Muebles',
+        'company_address'=> '9 de Julio 1449, Ctes, Argentina',
+        'phone'         => '+54 9 3794 098511',
+        'social_links'  => [
+            'facebook'  => 'https://fb.com/misitio',
+            'twitter'   => 'https://twitter.com/misitio',
+            'instagram' => 'https://instagram.com/misitio',
+        ],
+        'current_year'  => date('Y') // Usa el helper 'date'
+        ];
+
+        // 2. Construye la vista final ensamblando todo
+        return view('front/navbar')
+            . view('plantilla', ['title' => 'CVA Muebles'])
+            . view('front/footer', $footerData);
     }
 }
--- app/Views/plantilla.php
+++ app/Views/plantilla.php
@@ -4,67 +4,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title>CVA MUEBLES</title>
+    <title><?= $title ?? 'titulo variable' ?></title>
     
     <link rel="stylesheet" href="assets/css/miestilo.css">
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <script src="assets/js/bootstrap.bundle.min.js" ></script>
-  
+    
 </head>
-  <body>
-    <!--BARRA DE NAVEGACION-->
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid">
-
-        <!-- Logo de la barra de navegacion -->
-        <div class="content-logo">
-          <img src="assets/img/logo/cva2.png" alt="" width="50px" class="logo ms-2 me-2">
-        </div>
-        
-        <a class="navbar-brand" href="#"><span class="titulo-logo">CVA Muebles</span></a>
-
-        <!-- Botón para dispositivos pequeños -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" 
-          aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <!-- Contenedor del menú -->
-        <div class="collapse navbar-collapse justify-content-between" id="navbarContent">
-          
-          <!-- Menú centrado -->
-          <ul class="navbar-nav mx-auto">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Inicio</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Productos</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Ofertas</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Más Información</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contactanos</a>
-            </li>
-          </ul>
-
-          <!-- Links a la derecha -->
-          <div class="d-flex">
-            <a class="nav-link" href="#">
-              <img src="assets/img/iconos/cart-check.svg" alt="" class="icono">
-            </a>
-            
-            <a class="nav-link" href="#">
-              <img src="assets/img/iconos/person.svg" alt="" class="icono">
-            </a>
-          </div>
-        </div>
-
-      </div>
-    </nav>
-
+<body>
+  <div class= "body"> 
+    <h1>facu putooooo</p>
+  </div>
+  
   </body>
 </html>
