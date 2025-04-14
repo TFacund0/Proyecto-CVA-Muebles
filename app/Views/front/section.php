2a390d86c1626468acdcadd5db1aaba76b71545d
fer <fernandoarceobregon@gmail.com>
2025-04-13 22:33:48 -0300

    soy un capo facu comeme las pelotas

--- app/Views/front/section.php
+++ app/Views/front/section.php
@@ -6,29 +6,33 @@
     </div>
     
     <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="assets/img/fondos-carrusel/GojoWorld.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
-            </div>
-        </div>
-        
-        <div class="carousel-item">
-            <img src="" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
-            </div>
-        </div>
-        
-        <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>Some representative placeholder content for the third slide.</p>
-            </div>
-        </div>
+  <!-- Slide 1  -->
+  <div class="carousel-item active">
+    <img src="assets/img/fondos-carrusel/GojoWorld.jpg" class="d-block w-100" alt="Mueble artesanal">
+    <div class="carousel-caption d-none d-md-block" style="top: 20%; left: 10%;">
+      <h5 class="font-playfair display-4 text-uppercase fw-bold">Muebles con Alma</h5>
+      <p class="font-lora fs-5">Tallados a mano con maderas nobles de la región</p>
+    </div>
+  </div>
+  
+  <!-- Slide 2  -->
+  <div class="carousel-item">
+    <img src="assets/img/fondos-carrusel/GojoWorld.jpg" class="d-block w-100" alt="Taller de carpintería">
+    <div class="carousel-caption d-none d-md-block" style="bottom: 15%; right: 5%;">
+      <h5 class="font-montserrat fs-2 fw-light letter-spacing-2">Artesanía Tradicional</h5>
+      <p class="font-montserrat fs-6">Técnicas transmitidas por generaciones</p>
+    </div>
+  </div>
+  
+  <!-- Slide 3 -->
+  <div class="carousel-item">
+    <img src="assets/img/fondos-carrusel/GojoWorld.jpg" class="d-block w-100" alt="Proyecto terminado">
+    <div class="carousel-caption d-none d-md-block" style="top: 50%; left: 50%; transform: translate(-50%, -50%);">
+      <h5 class="font-raleway fs-1 fw-bold text-shadow">Hecho para Durar</h5>
+      <p class="font-raleway fs-5 text-shadow">Calidad que trasciende el tiempo</p>
+    </div>
+  </div>
+</div>
     </div>
     
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
