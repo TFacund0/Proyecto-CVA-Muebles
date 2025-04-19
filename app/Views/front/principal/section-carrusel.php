6bd1c82e6250245a65e73828f1d10b736696c818
Tobias <tobiascesarfacundoacevedo@gmail.com>
2025-04-19 14:53:09 -0300

    Incluye cambios en el carrusel, pero no está finalizado

--- app/Views/front/principal/section-carrusel.php
+++ app/Views/front/principal/section-carrusel.php
@@ -1,15 +1,20 @@
-<section id="carouselExampleCaptions" class="carousel slide">
+
+<!-- Carrusel -->
+<section id="carouselExampleCaptions" class="carousel slide" height="500px">
+    
+    <!-- Botones del Carrusel para Slide-->
     <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
     
-    <div class="carousel-inner">
+    <div class="carousel-inner" >
         <!-- Slide 1  -->
         <div class="carousel-item active">
-            <img src="assets/img/fondos-carrusel/taller.jpg" class="contain w-100" alt="">
-            <div class="carousel-caption d-none d-md-block text-content-carrusel">
+            <img src="assets/img/fondos-carrusel/taller.jpg" class="contain w-100" alt="Taller de carpintería">
+            
+            <div class="carousel-caption d-none d-md-block text-content-carrusel position-carrusel">
                 <h4 class="col text-carrusel">Bienvenidos a CVA Muebles</h4>
                 <p class="col text-carrusel">Diseño y fabricación artesanal de muebles de madera a medida</p>
 
@@ -21,19 +26,21 @@
         
         <!-- Slide 2  -->
         <div class="carousel-item">
-            <img src="assets/img/fondos-carrusel/GojoWorld.jpg" class="d-block w-100" alt="Taller de carpintería">
-            <div class="carousel-caption d-none d-md-block" style="bottom: 15%; right: 5%;">
-                <h5>Calidad artesanal, diseño con alma</h5>
-                <p>Cada pieza es única y hecha con dedicación</p>
+            <img src="assets/img/fondos-carrusel/taller.jpg" class="contain w-100" alt="Taller de carpintería">
+            
+            <div class="carousel-caption d-none d-md-block text-content-carrusel position-carrusel">
+                <h4 class="mt-3 col text-carrusel">Calidad artesanal, diseño con alma</h5>
+                <p class="col text-carrusel">Cada pieza es única y hecha con dedicación</p>
             </div>
         </div>
         
         <!-- Slide 3 -->
         <div class="carousel-item">
-            <img src="assets/img/fondos-carrusel/GojoWorld.jpg" class="d-block w-100" alt="Proyecto terminado">
-            <div class="carousel-caption d-none d-md-block" style="top: 50%; left: 50%; transform: translate(-50%, -50%);">
-                <h5>Pasión que se transmite</h5>
-                <p>Más de [X] años creando muebles con amor y oficio familiar</p>
+            <img src="assets/img/fondos-carrusel/taller.jpg" class="contain w-100" alt="Taller de carpintería">
+            
+            <div class="carousel-caption d-none d-md-block text-content-carrusel position-carrusel">
+                <h4 class="mt-3 col text-carrusel">Pasión que se transmite</h5>
+                <p class="col text-carrusel">Más de [X] años creando muebles con amor y oficio familiar</p>
             </div>
         </div>
     </div>
