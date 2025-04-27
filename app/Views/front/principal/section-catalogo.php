f7578f77ee64132de71fc7bc1ccf39ab86c0a054
Tobias <tobiascesarfacundoacevedo@gmail.com>
2025-04-27 10:50:13 -0300

    Incluye cambios en la identación de las secciones

--- app/Views/front/principal/section-catalogo.php
+++ app/Views/front/principal/section-catalogo.php
@@ -1,41 +1,42 @@
 <!-- Sección de Categorias/Catalogo -->
 <section class="container my-5 catalogo-content section-categorias">
-            <h2 class="titulo">Nuestras Especialidades</h2>
-            
-            <div class="row p-3">
-            <!-- Especialidad: Muebles de Sala -->
-                <div class="col-md-4 my-2">
-                    <div class="card catalogo-card h-100 text-center p-4 muebles-card ">
-                        <div class="card-body text-card">
-                            <h3>Muebles de Sala</h3>
-                            <p>Sofás, mesas de centro y libreros diseñados para tu comodidad</p>
-                            <a href="#" class="btn btn-outline-warning">Explorar</a>
-                        </div>
-                    </div>
+    <h2 class="titulo">Nuestras Especialidades</h2>
+    
+    <div class="row p-3">
+    <!-- Especialidad: Muebles de Sala -->
+        <div class="col-md-4 my-2">
+            <div class="card catalogo-card h-100 text-center p-4 muebles-card ">
+                <div class="card-body text-card">
+                    <h3>Muebles de Sala</h3>
+                    <p>Sofás, mesas de centro y libreros diseñados para tu comodidad</p>
+                    <a href="#" class="btn btn-outline-warning">Explorar</a>
                 </div>
-
-            <!-- Especialidad: Dormitorios -->
-                <div class="col-md-4 my-2">
-                    <div class="card catalogo-card h-100 text-center p-4 dormitorios-card">
-                        <div class="card-body text-card">
-                            <h3>Dormitorios</h3>
-                            <p>Camas, mesitas y armarios que transformarán tu espacio</p>
-                            <a href="#" class="btn btn-outline-warning">Explorar</a>
-                        </div>
-                    </div>
-                </div>                
-                
-            <!-- Especialidad: Cocina -->
-                <div class="col-md-4 my-2">
-                    <div class="card catalogo-card h-100 text-center p-4 cocina-card">
-                        <div class="card-body text-card">
-                            <h3>Cocina</h3>
-                            <p>Islas, alacenas y mesas rústicas para tu hogar</p>
-                            <a href="#" class="btn btn-outline-warning">Explorar</a>
-                        </div>
-                    </div>
-                </div>     
             </div>
+        </div>
+
+    <!-- Especialidad: Dormitorios -->
+        <div class="col-md-4 my-2">
+            <div class="card catalogo-card h-100 text-center p-4 dormitorios-card">
+                <div class="card-body text-card">
+                    <h3>Dormitorios</h3>
+                    <p>Camas, mesitas y armarios que transformarán tu espacio</p>
+                    <a href="#" class="btn btn-outline-warning">Explorar</a>
+                </div>
+            </div>
+        </div>                
+        
+    <!-- Especialidad: Cocina -->
+        <div class="col-md-4 my-2">
+            <div class="card catalogo-card h-100 text-center p-4 cocina-card">
+                <div class="card-body text-card">
+                    <h3>Cocina</h3>
+                    <p>Islas, alacenas y mesas rústicas para tu hogar</p>
+                    <a href="#" class="btn btn-outline-warning">Explorar</a>
+                </div>
+            </div>
+        </div>  
+        
+    </div>
 </section>
 
 <!-- Texto a modo de promoción de los productos y de la empresa -->
